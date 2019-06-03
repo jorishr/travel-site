25ffd10970bc3af3a0a00fecb2c3217335edaabd
jorishr <joris@jorisr.com>
2019-06-04 00:38:36 +0200

    sprite automation setup and icon updates completed

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,9 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     postcss = require('gulp-postcss'),
-    webpack = require('webpack');
+    webpack = require('webpack'),    
+    gulpSprite = require('gulp-svg-sprite'),
+    del = require('del');
     
 sass.compiler = require('node-sass');
 
@@ -23,6 +25,8 @@ function styles(){
         .pipe(gulp.dest('./app/'));
 }
 
+// NOTE: the order in the array of CSS tasks matters!
+
 function jsCompile(cb){
     webpack(require('./webpack.config.js'), function(err, stats){
         if (err){console.log(err.toString());};
@@ -31,7 +35,28 @@ function jsCompile(cb){
     });
 }
 
-// NOTE: the order in the array of CSS tasks matters!
+let spriteConfig = {
+    svg: {
+        xmlDeclaration: false,
+        doctypeDeclaration: false,
+        namespaceIDs: false,
+        namespaceClassnames: false
+    },
+    mode: {
+        symbol: true
+    }
+}
+
+function beginClean(){
+    return del('./app/src/images/sprite/');
+}
+
+function createSprite(){
+    return gulp.src('./app/src/images/icons/**/*.svg')
+    .pipe(gulpSprite(spriteConfig))
+    .pipe(gulp.dest('./app/src/images/sprite/'));
+}
+
 // NOTE: browserSync setting with Vagrant: proxy: 'travel.local', port:80
 
 function watchFiles(){
@@ -44,10 +69,13 @@ function watchFiles(){
     const mainFiles = ['./app/index.html', './app/main.css', './app/bundle.js']
     gulp.watch('./app/src/styles/**/*.scss', styles);
     gulp.watch('./app/src/scripts/**/*.js', jsCompile);
+    gulp.watch('./app/src/images/icons/**/*.svg', gulp.series(beginClean, createSprite));
     gulp.watch(mainFiles).on('change', reload);
 };
 
 exports.styles = styles;
 exports.jsCompile = jsCompile;
 exports.watch = watchFiles;
+exports.createSprite = createSprite;
+exports.beginClean = beginClean;
 
